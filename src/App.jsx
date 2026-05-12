1559bc3035683ed4042e7aa82dc512b317a5cf05
Ivan Sibalic <ivansibalicbm@gmail.com>
2026-05-12 19:34:49 +0200

    feat: wire Gallery, Reviews, Location into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
+import Gallery from './components/Gallery.jsx'
 import Services from './components/Services.jsx'
 import WhyUs from './components/WhyUs.jsx'
+import Reviews from './components/Reviews.jsx'
 import Booking from './components/Booking.jsx'
+import Location from './components/Location.jsx'
 import Footer from './components/Footer.jsx'
 
 export default function App() {
@@ -11,9 +14,12 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        <Gallery />
         <Services />
         <WhyUs />
+        <Reviews />
         <Booking />
+        <Location />
       </main>
       <Footer />
     </div>
